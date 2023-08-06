9e64ce0f969eb5a39f8ac2c75bd5b70792885fe4
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-06 21:58:44 +0300

    Update username placeholder

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :coins
+  # resources :coins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "home#index"
+
+  resources :coins
+  root 'coins#index'
+
 end
